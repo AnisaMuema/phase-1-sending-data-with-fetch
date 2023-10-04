5a7e01fca3f51fa9b1244657d974400b6c946983
anisamuema <anisa.muema.school@gmail.com>
2023-10-04 20:49:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+let FormData = {
+    name: nameInput,
+    email:emailInput,
+
+}
+
+function submitData(nameInput, emailInput){
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: nameInput,
+            email:emailInput,
+        }),
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json())
+    .then((json) => document.body.innerHTML = json.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
